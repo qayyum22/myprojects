6b9953b14877db24c7a6a1ae107bb4e8330c7c05
qayyum22 <54232882+qayyum22@users.noreply.github.com>
2023-08-28 09:16:03 +0530

    added sociohub project

---
+++ sociohub/client/src/App.js
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import { BrowserRouter, Navigate, Routes, Route} from 'react-router-dom';
+import HomePage from 'scenes/homePage';
+import LoginPage from 'scenes/loginPage';
+import ProfilePage from 'scenes/profilePage';
+import { useMemo } from 'react';
+import { UseSelector, useSelector } from 'react-redux/es/hooks/useSelector';
+import {CssBaseline, ThemeProvider} from '@mui/material';
+import { createTheme } from '@mui/material';
+import { themeSettings } from './theme';
+
+
+function App() {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(()=> createTheme(themeSettings(mode)), [mode]); 
+  const isAuth = Boolean(useSelector((state) => state.token)); 
+
+  return (
+    <div className="app">   
+      <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+      <Routes>
+        <Route path="/" element = {<LoginPage />}/>
+        <Route path="/home" element = {isAuth ? <HomePage /> : <Navigate to="/" />}/>
+        <Route path="/profile/:userId" element = {isAuth ? <ProfilePage /> : <Navigate to="/" /> }/>
+
+      </Routes>
+      </ThemeProvider>
+      </BrowserRouter>
+
+    </div>);
+}
+
+export default App;
